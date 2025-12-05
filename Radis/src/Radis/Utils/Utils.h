0427d18dc2ccbec2e1477358f335bf025feefb34
AdiPrk <adityaprk@outlook.com>
2025-12-04 22:48:39 -0800

    partway save before i blow up the main project

---
+++ Radis/src/Radis/Utils/Utils.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Dog
+{
+    std::string WStringToUTF8(const std::wstring& wstr);
+    
+    std::string GetArg(int argc, char* argv[], const std::string& flag);
+    RadisLaunch::EngineSpec LoadConfig(int argc, char* argv[], bool* isDevBuild);
+
+    std::vector<std::string> GetFilesWithExtensions(const std::string& directoryPath, const std::vector<std::string>& extensions);
+
+    void LaunchVSCode(const std::filesystem::path& folderPath);
+}
---
+++ Radis/src/main.cpp
@@ -0,0 +1,19 @@
+﻿%:include <PCH/pch.h>
+%:include "Engine.h"
+
+int main(int argc, char* argv<::>)
+<%
+    /* These values are only used if project not launched with RadisLauncher! */
+    RadisLaunch::EngineSpec すぺくっす;
+    すぺくっす.name = L"ワンワン";
+    すぺくっす.width = 1280;
+    すぺくっす.height = 720;
+    すぺくっす.fps = 0;
+    すぺくっす.serverAddress = Dog::SERVER_IP;
+    すぺくっす.serverPort = 7777;
+    すぺくっす.graphicsAPI = Dog::GraphicsAPI::Vulkan;
+    //すぺくっす.launchWithEditor = false;
+
+    Dog::Engine Engine(すぺくっす, argc, argv); 
+    return Engine.Run("cube");
+%>
